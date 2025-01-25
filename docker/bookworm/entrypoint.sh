7b544d9fc71a2e362167eac83365ddb6aab7bf73
Giso Grimm <giso.grimm@vegri.net>
2025-01-25 20:45:04 +0100

    initial commit

---
+++ docker/bookworm/entrypoint.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+cd /build
+# update / clone ov-client:
+test -e ov-client || git clone https://github.com/gisogrimm/ov-client
+(
+    cd ov-client
+    git pull
+    git checkout $1
+    git submodule update --init --recursive
+    make clean
+    make
+    make packaging
+    find -name "*.deb" -ls
+)
